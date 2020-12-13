736d8f18e6c8d94200bf86b0ab86126071c6e9fe
Alexander Yakimov <{ID}+{username}@users.noreply.github.com>
2020-12-13 18:02:11 +0400

    Add simple example that works with S3 terraform backend

---
+++ try-terraform/firtst-example.tf
@@ -0,0 +1,20 @@
+// For security reasons there is no real backend config in public repo.
+//You can configure it by uncommenting the next terraform statement
+//terraform {
+//  backend "s3" {
+//    bucket = "Specify your bucket here"
+//    key    = "Specify your key here"
+//    region = "Specify your region here"
+//    profile = "your aws profile"
+//  }
+//}
+
+provider "aws" {
+  profile = "tf"
+  region  = "eu-west-1"
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-0ce1e3f77cd41957e"
+  instance_type          = "t2.micro"
+}
